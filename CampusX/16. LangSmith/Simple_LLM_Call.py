f4a0680371a3558e3b68fcff07c8c99112bc329a
Swapnil <swpnltiwari361@gmail.com>
2026-01-24 18:29:14 +0000

    New File with tags and metadata

--- CampusX/16. LangSmith/Simple_LLM_Call.py
+++ CampusX/16. LangSmith/Simple_LLM_Call.py
@@ -1,8 +1,9 @@
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
+import os
 
 model = init_chat_model('gpt-5-nano')
 
-response = model.invoke("What is India Doing in Quantum Computing Space?")
+response = model.invoke("What is India Doing in Quantum Computing Space?')
 
 print(response.content)
